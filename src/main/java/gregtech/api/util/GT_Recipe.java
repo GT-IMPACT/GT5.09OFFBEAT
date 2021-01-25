9cd6186d4eeefba2affd1b18d5e48e4b8294ff34
Lex <domian844@gmail.com>
2021-01-25 16:04:46 +0700

    fix map nq cyclone

--- src/main/java/gregtech/api/util/GT_Recipe.java
+++ src/main/java/gregtech/api/util/GT_Recipe.java
@@ -660,7 +660,7 @@ public class GT_Recipe implements Comparable<GT_Recipe> {
 
         public static final GT_Recipe_Map_Fuel sLiquidNqGenerator = new GT_Recipe_Map_Fuel(new HashSet<GT_Recipe>(25), "gt.recipe.liquidnaquadahgenerator", "Liquid Nq Generator", null, RES_PATH_GUI + "NaquadahGenerator", 1, 1, 0, 0, 1, "Fuel Value: ", 1, " EU for 1 Amp", true, false);
         public static final GT_Recipe_Map_Fuel sLiquidENqGenerator = new GT_Recipe_Map_Fuel(new HashSet<GT_Recipe>(25), "gt.recipe.liquidenaquadahengenerator", "Liquid Enriched Nq Generator", null, RES_PATH_GUI + "NaquadahGenerator", 1, 1, 0, 0, 1, "Fuel Value: ", 1, " EU for 1 Amp", true, false);
-        public static final GT_Recipe_Map sCyclonRecipes = new GT_Recipe_Map(new HashSet<GT_Recipe>(1000), "gt.recipe.cyclone", "Heavy Metal Cyclone", null, RES_PATH_GUI + "basicmachines/Default", 6, 6, 0, 0, 3, E, 1, E, true, false);
+        public static final GT_Recipe_Map sCyclonRecipes = new GT_Recipe_Map(new HashSet<GT_Recipe>(1000), "gt.recipe.cyclone", "Heavy Metal Cyclone", null, RES_PATH_GUI + "basicmachines/Default", 6, 6, 0, 0, 1, E, 1, E, true, false);
         public static final GT_Recipe_Map_Fuel sHyperGenerator = new GT_Recipe_Map_Fuel(new HashSet<GT_Recipe>(25), "gt.recipe.hypergenerator", "Hyper Generator", null, RES_PATH_GUI + "NaquadahGenerator", 1, 1, 0, 0, 1, "Fuel Value: ", 1, " EU for 1 Amp", true, false);
 
 
