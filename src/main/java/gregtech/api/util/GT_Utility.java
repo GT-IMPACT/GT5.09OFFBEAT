f05371bb3c886f303b4c561b1af545e0bac8d9b4
Lex <domian844@gmail.com>
2021-02-03 02:33:21 +0700

    BasicMachine fluid tank manipulations in GU

--- src/main/java/gregtech/api/util/GT_Utility.java
+++ src/main/java/gregtech/api/util/GT_Utility.java
@@ -851,6 +851,15 @@ public class GT_Utility {
         return rStack;
     }
 
+    public static FluidStack getFluidFromDisplayStack(ItemStack aDisplayStack) {
+        if (!isStackValid(aDisplayStack) ||
+                aDisplayStack.getItem() != ItemList.Display_Fluid.getItem() ||
+                !aDisplayStack.hasTagCompound())
+            return null;
+        Fluid tFluid = FluidRegistry.getFluid(ItemList.Display_Fluid.getItem().getDamage(aDisplayStack));
+        return new FluidStack(tFluid, (int) aDisplayStack.getTagCompound().getLong("mFluidDisplayAmount"));
+    }
+
     public static boolean containsFluid(ItemStack aStack, FluidStack aFluid, boolean aCheckIFluidContainerItems) {
         if (isStackInvalid(aStack) || aFluid == null) return false;
         if (aCheckIFluidContainerItems && aStack.getItem() instanceof IFluidContainerItem && ((IFluidContainerItem) aStack.getItem()).getCapacity(aStack) > 0)
@@ -2475,6 +2484,18 @@ public class GT_Utility {
 
     public static boolean isPartOfOrePrefix(ItemStack aStack, OrePrefixes aPrefix){
         return GT_OreDictUnificator.getAssociation(aStack) != null ? GT_OreDictUnificator.getAssociation(aStack).mPrefix.equals(aPrefix) : false;
+    }
+
+    /**
+     * Add an itemstack to player inventory, or drop on ground if full.
+     * Can be called on client but it probably won't work very well.
+     */
+    public static void addItemToPlayerInventory(EntityPlayer aPlayer, ItemStack aStack) {
+        if (isStackInvalid(aStack)) return;
+        if (!aPlayer.inventory.addItemStackToInventory(aStack) && !aPlayer.worldObj.isRemote) {
+            EntityItem dropItem = aPlayer.entityDropItem(aStack, 0);
+            dropItem.delayBeforeCanPickup = 0;
+        }
     }
 
 }
