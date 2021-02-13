65f409d163f551776fbcce3b949f2c683e84f4b3
Max <maxim79135@gmail.com>
2021-02-13 10:32:24 +0300

    Added spaces in the tooltip

--- src/main/java/gregtech/common/tileentities/machines/multi/GT_MetaTileEntity_BrickedBlastFurnace.java
+++ src/main/java/gregtech/common/tileentities/machines/multi/GT_MetaTileEntity_BrickedBlastFurnace.java
@@ -33,7 +33,7 @@ public class GT_MetaTileEntity_BrickedBlastFurnace extends GT_MetaTileEntity_Pri
                 "Useable for Steel and general Pyrometallurgy",
                 "Size(WxHxD): 3x4x3 (Hollow, with opening on top)",
                 "Built from 32 Fired Brick Blocks",
-                "Causes"+ setPollut() +"Pollution per second"};
+                "Causes "+ setPollut() +" Pollution per second"};
 }
 
     public ITexture[] getTexture(IGregTechTileEntity aBaseMetaTileEntity, byte aSide, byte aFacing, byte aColorIndex, boolean aActive, boolean aRedstone) {
