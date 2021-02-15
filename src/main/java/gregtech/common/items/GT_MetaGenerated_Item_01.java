3296b08819b89e40c8592b39b17cee86bea5669b
Lex <domian844@gmail.com>
2021-02-16 01:14:19 +0700

    wops. fix

--- src/main/java/gregtech/common/items/GT_MetaGenerated_Item_01.java
+++ src/main/java/gregtech/common/items/GT_MetaGenerated_Item_01.java
@@ -565,13 +565,13 @@ public class GT_MetaGenerated_Item_01 extends GT_MetaGenerated_Item_X32 {
 
         ItemList.Steam_Valve_LV.set(addItem(tLastID = 660, "LV Steam Regulator", "Configuable up to 20.480 L/sec (as Cover)"));
         ItemList.Steam_Valve_MV.set(addItem(tLastID = 661, "MV Steam Regulator", "Configuable up to 81.920 L/sec (as Cover)"));
-        ItemList.Steam_Valve_MV.set(addItem(tLastID = 662, "HV Steam Regulator", "Configuable up to 327.680 L/sec (as Cover)"));
+        ItemList.Steam_Valve_HV.set(addItem(tLastID = 662, "HV Steam Regulator", "Configuable up to 327.680 L/sec (as Cover)"));
         ItemList.Steam_Valve_EV.set(addItem(tLastID = 663, "EV Steam Regulator", "Configuable up to 1.310.720 L/sec (as Cover)"));
         ItemList.Steam_Valve_IV.set(addItem(tLastID = 664, "IV Steam Regulator", "Configuable up to 5.242.880 L/sec (as Cover)"));
 
         GregTech_API.registerCover(ItemList.Steam_Valve_LV.get(1L), new GT_MultiTexture(new ITexture[]{Textures.BlockIcons.MACHINE_CASINGS[1][0], new GT_RenderedTexture(Textures.BlockIcons.OVERLAY_PUMP)}), new GT_Cover_FluidRegulator(1024, true));
         GregTech_API.registerCover(ItemList.Steam_Valve_MV.get(1L), new GT_MultiTexture(new ITexture[]{Textures.BlockIcons.MACHINE_CASINGS[2][0], new GT_RenderedTexture(Textures.BlockIcons.OVERLAY_PUMP)}), new GT_Cover_FluidRegulator(4096, true));
-        GregTech_API.registerCover(ItemList.Steam_Valve_MV.get(1L), new GT_MultiTexture(new ITexture[]{Textures.BlockIcons.MACHINE_CASINGS[3][0], new GT_RenderedTexture(Textures.BlockIcons.OVERLAY_PUMP)}), new GT_Cover_FluidRegulator(16384, true));
+        GregTech_API.registerCover(ItemList.Steam_Valve_HV.get(1L), new GT_MultiTexture(new ITexture[]{Textures.BlockIcons.MACHINE_CASINGS[3][0], new GT_RenderedTexture(Textures.BlockIcons.OVERLAY_PUMP)}), new GT_Cover_FluidRegulator(16384, true));
         GregTech_API.registerCover(ItemList.Steam_Valve_EV.get(1L), new GT_MultiTexture(new ITexture[]{Textures.BlockIcons.MACHINE_CASINGS[4][0], new GT_RenderedTexture(Textures.BlockIcons.OVERLAY_PUMP)}), new GT_Cover_FluidRegulator(65536, true));
         GregTech_API.registerCover(ItemList.Steam_Valve_IV.get(1L), new GT_MultiTexture(new ITexture[]{Textures.BlockIcons.MACHINE_CASINGS[5][0], new GT_RenderedTexture(Textures.BlockIcons.OVERLAY_PUMP)}), new GT_Cover_FluidRegulator(262144, true));
 
