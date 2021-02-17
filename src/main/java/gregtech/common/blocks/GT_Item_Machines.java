0de24a3309c3fd411f6e09631fbd135ae3b0bbd9
Max <maxim79135@gmail.com>
2021-02-17 18:42:36 +0300

    Added stored liquid in desctiption

--- src/main/java/gregtech/common/blocks/GT_Item_Machines.java
+++ src/main/java/gregtech/common/blocks/GT_Item_Machines.java
@@ -18,7 +18,9 @@ import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.EnumChatFormatting;
 import net.minecraft.world.World;
 
+import java.text.NumberFormat;
 import java.util.List;
+import java.util.Locale;
 
 import static net.minecraft.util.EnumChatFormatting.*;
 
@@ -90,6 +92,14 @@ public class GT_Item_Machines
                 }
                 if (aNBT.getBoolean("mSteamConverter")) {
                     aList.add(GT_LanguageManager.addStringLocalization("GT_TileEntity_STEAMCONVERTER", "has Steam Upgrade", !GregTech_API.sPostloadFinished));
+                }
+                NBTTagCompound tFluidAmount = aNBT.getCompoundTag("mFluid");
+                if (tFluidAmount != null) {
+                    String fluidName = tFluidAmount.getString("FluidName");
+                    int amountFluid;
+                    if ((amountFluid = tFluidAmount.getInteger("Amount")) > 0) {
+                        aList.add("Stored " + BLUE + NumberFormat.getNumberInstance().format((int) amountFluid) + "L" + GRAY + " of " + fluidName);
+                    }
                 }
                 int tAmount;
                 if ((tAmount = aNBT.getByte("mSteamTanks")) > 0) {
