c61630da53fa57b7e0d7854c74227ee962d8898c
Спартак <spartak_korolev@mail.ru>
2024-01-02 17:41:50 +0300

    Fixed mistake
    lol

--- src/main/java/gregtech/common/items/GT_MetaGenerated_Item_03.java
+++ src/main/java/gregtech/common/items/GT_MetaGenerated_Item_03.java
@@ -335,8 +335,8 @@ public class GT_MetaGenerated_Item_03
         ItemList.Circuit_Cosmiccomputer.set(addItem(tLastID = 175, "Cosmic Processor Assembly", "A Nano Circuit (UIV)",  new Object[]{OrePrefixes.circuit.get(Materials.Bio), SubTag.NO_UNIFICATION}));
         ItemList.Circuit_Cosmicsupercomputer.set(addItem(tLastID = 176, "Cosmic Supercomputer", "A Piko Circuit (UMV)",  new Object[]{OrePrefixes.circuit.get(Materials.Nano), SubTag.NO_UNIFICATION}));
 
-        ItemList.NaquaBoardEmpty.set(addItem(tLastID = 177, "Portable Naquadah Reactor (Empty)", "Infinity enegry for high consuption circuits"));
-        ItemList.NaquaBoardFull.set(addItem(tLastID = 178, "Portable Naquadah Reactor (Full)", "Infinity enegry for high consuption circuits"));
+        ItemList.NaquaBoardEmpty.set(addItem(tLastID = 177, "Portable Naquadah Reactor (Empty)", "Infinity energy for high consuption circuits"));
+        ItemList.NaquaBoardFull.set(addItem(tLastID = 178, "Portable Naquadah Reactor (Full)", "Infinity energy for high consuption circuits"));
 
 
     }
