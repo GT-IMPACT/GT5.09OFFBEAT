4a718808c5f9de1a55936eb6c29da3d9f5142582
Alexey Grishankov <domian844@gmail.com>
2024-05-19 17:28:32 +0300

    fix plasma turbine ArithmeticException

--- src/main/java/gregtech/common/tileentities/machines/multi/GT_MetaTileEntity_LargeTurbine_Plasma.java
+++ src/main/java/gregtech/common/tileentities/machines/multi/GT_MetaTileEntity_LargeTurbine_Plasma.java
@@ -1,18 +1,11 @@
 package gregtech.common.tileentities.machines.multi;
 
-import java.util.ArrayList;
-import java.util.Collection;
-
 import gregtech.api.GregTech_API;
 import gregtech.api.enums.Textures;
 import gregtech.api.interfaces.ITexture;
 import gregtech.api.interfaces.metatileentity.IMetaTileEntity;
 import gregtech.api.interfaces.tileentity.IGregTechTileEntity;
-import gregtech.api.items.GT_MetaGenerated_Tool;
-import gregtech.api.metatileentity.implementations.GT_MetaTileEntity_Hatch;
-
 import gregtech.api.render.TextureFactory;
-import gregtech.api.util.GT_Log;
 import gregtech.api.util.GT_Recipe;
 import gregtech.api.util.GT_Recipe.GT_Recipe_Map;
 import gregtech.api.util.GT_Utility;
@@ -20,6 +13,7 @@ import net.minecraft.block.Block;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fluids.FluidRegistry;
 import net.minecraftforge.fluids.FluidStack;
+
 import java.util.ArrayList;
 import java.util.Collection;
 
@@ -94,38 +88,37 @@ public class GT_MetaTileEntity_LargeTurbine_Plasma extends GT_MetaTileEntity_Lar
 
         int actualOptimalFlow = 0;
 
-        if (aFluids.size() >= 1) {
+        if (!aFluids.isEmpty()) {
 
             FluidStack firstFuelType = new FluidStack(aFluids.get(0), 0); // Identify a SINGLE type of fluid to process.  Doesn't matter which one. Ignore the rest!
             int fuelValue = getFuelValue(firstFuelType);
-            actualOptimalFlow = (int) ((aOptFlow + fuelValue - 1) / fuelValue);
+
+            actualOptimalFlow = GT_Utility.safeInt((long) Math.ceil((double) (aOptFlow + fuelValue - 1) / (double) fuelValue));
             this.realOptFlow = actualOptimalFlow; // For scanner info
 
-            int remainingFlow = (int) (actualOptimalFlow * 1.25f); // Allowed to use up to 125% of optimal flow.  Variable required outside of loop for multi-hatch scenarios.            int flow = 0;
+            int remainingFlow =  GT_Utility.safeInt((long) (actualOptimalFlow * 1.25f)); // Allowed to use up to 125% of optimal flow.  Variable required outside of loop for multi-hatch scenarios.            int flow = 0;
             int flow = 0;
             int totalFlow = 0;
 
-
-            int aFluids_sS=aFluids.size();
-            for (int i = 0; i < aFluids_sS; i++) {
-                if (aFluids.get(i).isFluidEqual(firstFuelType)) {
-                    flow = aFluids.get(i).amount; // Get all (steam) in hatch
+            for (FluidStack aFluid : aFluids) {
+                if (aFluid.isFluidEqual(firstFuelType)) {
+                    flow = aFluid.amount; // Get all (steam) in hatch
                     flow = Math.min(flow, Math.min(remainingFlow, (int) (actualOptimalFlow * 1.25f))); // try to use up to 125% of optimal flow w/o exceeding remainingFlow
-                    depleteInput(new FluidStack(aFluids.get(i), flow)); // deplete that amount
-                    this.storedFluid = aFluids.get(i).amount;
+                    depleteInput(new FluidStack(aFluid, flow)); // deplete that amount
+                    this.storedFluid = aFluid.amount;
                     remainingFlow -= flow; // track amount we're allowed to continue depleting from hatches
                     totalFlow += flow; // track total input used
                 }
             }
             String fn = FluidRegistry.getFluidName(firstFuelType);
-            String[] nameSegments = fn.split("\\.",2);
-            if (nameSegments.length==2){
-                String outputName=nameSegments[1];
+            String[] nameSegments = fn.split("\\.", 2);
+            if (nameSegments.length == 2) {
+                String outputName = nameSegments[1];
                 FluidStack output = FluidRegistry.getFluidStack(outputName, totalFlow);
-                if (output==null){
-                    output = FluidRegistry.getFluidStack("molten."+outputName, totalFlow);
+                if (output == null) {
+                    output = FluidRegistry.getFluidStack("molten." + outputName, totalFlow);
                 }
-                if (output!=null) {
+                if (output != null) {
                     addOutput(output);
                 }
             }
@@ -135,13 +128,15 @@ public class GT_MetaTileEntity_LargeTurbine_Plasma extends GT_MetaTileEntity_Lar
 
             if (totalFlow != actualOptimalFlow) {
                 float efficiency = 1.0f - Math.abs(((totalFlow - (float) actualOptimalFlow) / actualOptimalFlow));
-                if(totalFlow>actualOptimalFlow){efficiency = 1.0f;}
+                if (totalFlow > actualOptimalFlow) {
+                    efficiency = 1.0f;
+                }
                 if (efficiency < 0)
                     efficiency = 0; // Can happen with really ludicrously poor inefficiency.
                 tEU *= efficiency;
-                tEU = Math.max(1, (int)((long)tEU * (long)aBaseEff / 10000L));
+                tEU = Math.max(1, (int) ((long) tEU * (long) aBaseEff / 10000L));
             } else {
-                tEU = (int)((long)tEU * (long)aBaseEff / 10000L);
+                tEU = (int) ((long) tEU * (long) aBaseEff / 10000L);
             }
 
             return tEU;
