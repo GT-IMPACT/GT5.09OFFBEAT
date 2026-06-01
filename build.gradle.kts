75180c16d7bce3c37184b2f230846db16930e116
Grishankov Alexey <domian844@gmail.com>
2026-06-01 09:08:08 +0300

    update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,32 +25,42 @@ repositories {
     maven("https://maven.accident.space/repository/maven-public/") {
         mavenContent {
             includeGroup("space.impact")
+            includeGroup("com.github.GTNewHorizons")
             includeGroupByRegex("space\\.impact\\..+")
+        }
+        credentials {
+            username = System.getenv("MAVEN_USER") ?: "NONE"
+            password = System.getenv("MAVEN_PASSWORD") ?: "NONE"
         }
     }
 }
 
 dependencies {
-    api("space.impact:Impact-API:0.0.4:dev")
-    api("space.impact:VirtualWorld:2.0.0b.dirty:dev")
-    api("space.impact:Impact-core:1.1.0.24:dev") { isTransitive = false }
-    api("com.github.GTNewHorizons:NotEnoughItems:2.3.+:dev") { isChanging = true }
-    api("com.github.GTNewHorizons:ModularUI:1.1.10:dev")
+    // impact
+    api("space.impact:ImpactAPI:0.0.4:dev")
+    api("space.impact:VirtualWorld:2.0.0d.dirty:dev")
+    compileOnlyApi("space.impact:impact-core:1.1.0.25-1-gb6211b5.dirty:dev") { isTransitive = false }
+    runtimeOnlyNonPublishable("space.impact:VisualProspecting:1.3.2")
+
+    // maven impact
+    api("com.github.GTNewHorizons:CodeChickenCore:1.3.11:dev") {
+        version { strictly("1.3.11") }
+    }
+    api("com.github.GTNewHorizons:NotEnoughItems:2.6.0-GTNH:dev")
+    api("com.github.GTNewHorizons:modularui:1.1.24:dev")
+    api("com.github.GTNewHorizons:appliedenergistics2:rv3-beta-400-GTNH:dev")
+    api("com.github.GTNewHorizons:IC2NuclearControl:2.6.2:dev")
+    compileOnlyApi("com.github.GTNewHorizons:Translocator:1.1.2.21:dev")
+    compileOnlyApi("com.github.GTNewHorizons:chisel:2.11.0-GTNH")
+
+    // other
     api("net.industrial-craft:industrialcraft-2:2.2.828-experimental:dev")
-    api("com.github.GTNewHorizons:Applied-Energistics-2-Unofficial:rv3-beta-213-GTNH:dev")
-    api("com.github.GTNewHorizons:Nuclear-Control:2.4.+:dev") { isTransitive = false; isChanging = true }
-
-    compileOnly("com.github.GTNewHorizons:Chisel:2.11.0-GTNH:dev") { isTransitive = false }
-    compileOnly("com.github.GTNewHorizons:Translocators:1.1.2.21:dev") { isTransitive = false }
     compileOnly("curse.maven:cofh-core-69162:2388751") { isTransitive = false }
-
     compileOnlyApi("com.github.GTNewHorizons:AppleCore:3.3.4:dev") { isTransitive = false }
     compileOnlyApi("com.github.GTNewHorizons:BuildCraft:7.1.42:dev") { isTransitive = false }
     compileOnlyApi("com.github.GTNewHorizons:EnderIO:2.9.3:dev") { isTransitive = false }
     compileOnlyApi("com.github.GTNewHorizons:ForestryMC:4.10.1:dev") { isTransitive = false }
     compileOnlyApi("com.github.GTNewHorizons:ProjectRed:4.11.1-GTNH:dev") { isTransitive = false }
     compileOnlyApi("com.github.GTNewHorizons:Railcraft:9.16.3:dev") { isTransitive = false }
-
     runtimeOnlyNonPublishable("com.github.GTNewHorizons:DuraDisplay:1.3.2:dev")
-    runtimeOnlyNonPublishable("space.impact:VisualProspecting:1.3.2")
 }
